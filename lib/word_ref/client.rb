8a4f34e241284c1e03f7f061b4f9763caddd46cc
Dylan Markow <dylan@dylanmarkow.com>
2012-03-14 16:34:03 -0700

    Fix constant assignment

--- lib/word_ref/client.rb
+++ lib/word_ref/client.rb
@@ -2,12 +2,12 @@ require 'faraday'
 require 'faraday_middleware'
 
 module WordRef
+  API_KEY = ENV['WORDREF_API_KEY']
   class Client
     attr_accessor :result
     attr_accessor :conn, :response
 
     def initialize
-      API_KEY = ENV['WORDREF_API_KEY']
       @conn = Faraday.new(:url => "http://api.wordreference.com/#{API_KEY}/json") do |builder|
         builder.response :logger
         builder.use FaradayMiddleware::Mashify
